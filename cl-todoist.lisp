892cc6927d839eb6fe08251876834ae5e3a841ec
Mason Smith <masonium@masonium.com>
2012-07-26 18:21:03 -0700

    Added completed-items, sync methods

--- cl-todoist.lisp
+++ cl-todoist.lisp
@@ -10,14 +10,23 @@
         (json:*json-identifier-name-to-lisp* #'key-to-lisp))
     (json:decode-json-from-string json-string)))
 
+(define-constant +methods-base-map+
+    (alist-hash-table '(("sync-get" . "TodoistSync/v2")) :test 'equal))
+
+(define-constant +methods-map+
+    (alist-hash-table '(("sync-get" . "get")) :test 'equal))
 
 (defun request (method parameters &optional json-only)
   "Perform a standard TODOIST API call, returning the parsed JSON result."
   (let ((drakma:*text-content-types* '(("text") ("application" "json")))
         (response
-          (drakma:http-request (format nil "http://todoist.com/API/~A" method)
+          (drakma:http-request (format nil "http://todoist.com/~A/~A"
+                                       (gethash method +methods-base-map+ "API")
+                                       (gethash method +methods-map+ "method"))
                                :parameters parameters)))
     (if json-only
         response
         (decode-json-from-string* response))))
 
+(defun get-all (user)
+  (request "sync-get" `(("token" . ,(api-token user)))) t)
--- project.lisp
+++ project.lisp
@@ -41,3 +41,25 @@
                            (list (cons "token" (api-token user))
                                  (cons "project_id" (format nil "~A" (id project)))))))
     (mapcar (fn (json:make-object % 'item)) response))))
+
+(defun get-completed-items (user project &key interval label)
+  (let* ((method (if (and (null interval) (null label) project)
+                     "getCompletedItems"
+                     "getAllCompletedItems"))
+         (project-option
+            (when project
+              (let ((project-id
+                      (etypecase project
+                        (string project)
+                        (integer (format nil "~A" project))
+                        (project (format nil "~A" (id project))))))
+                (list (cons "project_id" project-id)))))
+         (interval-option (when interval (list (cons "interval" interval))))
+         (label-option (when label (list (cons "label" label))))
+         (response (request method
+                            (append (list (cons "token" (api-token user)))
+                                    project-option interval-option label-option))))
+    (mapcar (fn (json:make-object % 'item))
+            (if (string= method "getCompletedItems")
+                response
+                (cdr (assoc 'items response))))))
